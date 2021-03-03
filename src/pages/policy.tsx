1f13818713b1b48bfe7b9644f059724f08cf9660
yuki <yuki.onishi1129@gmail.com>
2021-03-03 22:31:10 +0900

    add: pages

---
+++ src/pages/policy.tsx
@@ -0,0 +1,37 @@
+/**
+ * プライバシーポリシー
+ * @package pages
+ */
+import React from 'react'
+import { css, SerializedStyles } from '@emotion/react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Styles from '@/styles/Home.module.scss'
+/* components */
+import { Header } from '@/components/layouts/Header'
+
+type Props = {
+  blogs: {
+    id: number
+    title: string
+  }[]
+}
+
+export default function Policy() {
+  const _css = css`
+    font-size: 44px;
+    color: blue;
+  `
+  // styleを上書きできる
+  const hello = (_css: SerializedStyles) => css`
+    color: red;
+    ${_css}
+  `
+
+  return (
+    <>
+      <Header />
+      <div css={hello(_css)}>テスト</div>
+    </>
+  )
+}
