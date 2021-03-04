1d67d7f8ac450954b66ece41087c10e71adffb00
yuki <yuki.onishi1129@gmail.com>
2021-03-04 22:20:38 +0900

    add: common components BaseFixedPageLayout

---
+++ src/components/layouts/BaseFicxedPageLayout/Presenter.tsx
@@ -0,0 +1,31 @@
+/**
+ * layouts/BaseFixedPageLayout
+ * PresentationalComponent
+ * @package Component
+ */
+import React from 'react'
+/* components */
+import { BaseLayout } from '@/components/layouts/BaseLayout'
+/* styles */
+import { styles } from './style'
+
+/**
+ * Props
+ */
+export type Props = {
+  children: React.ReactNode
+}
+
+/**
+ * presenter
+ * @param props
+ */
+export const Presenter: React.FC<Props> = (props: Props) => {
+  const { children } = props
+
+  return (
+    <BaseLayout>
+      <article css={styles.article}>{children}</article>
+    </BaseLayout>
+  )
+}
---
+++ src/components/layouts/BaseFicxedPageLayout/style.ts
@@ -0,0 +1,20 @@
+/**
+ * layouts/BaseFixedPageLayout
+ * StyleComponent
+ * @package Component
+ */
+import { css } from '@emotion/react'
+
+/**
+ * style
+ */
+const article = css`
+  width: 820px;
+`
+
+/**
+ * export styles
+ */
+export const styles = {
+  article,
+}
