c55af0da66c035fe8c63b55eb22e43e8fd62ab77
yuki <yuki.onishi1129@gmail.com>
2021-03-23 08:54:15 +0900

    add: show list blog item

--- src/components/pages/SearchTemplate/Presenter.tsx
+++ src/components/pages/SearchTemplate/Presenter.tsx
@@ -6,8 +6,10 @@
 import React from 'react'
 /* components */
 import { BaseLayout } from '@/components/layouts/BaseLayout'
-import { BreadList } from '@/components/layouts/BreadList'
 import { PageTitle } from '@/components/common/atoms/PageTitle'
+import { SearchBlogItem } from './organisms/SearchBlogItem'
+/* contexts */
+import { useBlogState } from '@/contexts/BlogContext'
 // /* constants */
 // import { blogShowCount } from '@/constants/config'
 /* types */
@@ -29,10 +31,18 @@ type Props = {
  */
 export const Presenter: React.FC<Props> = (props: Props) => {
   const { breadName } = props
+  const { blogList } = useBlogState()
+
   return (
     <BaseLayout breadName={breadName}>
       <div className={styles.container}>
         <PageTitle title="検索結果" />
+        <div className={styles.list}>
+          {blogList.length > 0 &&
+            blogList.map((blog) => (
+              <SearchBlogItem key={blog.id} blogItem={blog} />
+            ))}
+        </div>
       </div>
     </BaseLayout>
   )
---
+++ src/components/pages/SearchTemplate/organisms/SearchBlogItem/index.tsx
@@ -0,0 +1,35 @@
+/**
+ * SearchTemplate/organisms/SearchBlogItem
+ * ContainerConponent
+ * @package Component
+ */
+import React from 'react'
+/* components */
+import { Presenter } from './Presenter'
+/* types */
+import { BlogItemType } from '@/types/blog'
+import { ImageType } from '@/types/image'
+
+/**
+ * props
+ */
+type Props = {
+  blogItem: BlogItemType
+}
+
+/**
+ * container
+ * @param props Props
+ * @returns
+ */
+export const SearchBlogItem: React.FC<Props> = (props: Props) => {
+  const { blogItem } = props
+
+  const propsImage: ImageType = {
+    url: blogItem?.image?.url ? blogItem.image.url : '/no_image.png',
+    width: blogItem?.image?.width ? blogItem.image.width : 750,
+    height: blogItem?.image?.height ? blogItem.image.height : 422,
+  }
+
+  return <Presenter blogItem={blogItem} image={propsImage} />
+}
