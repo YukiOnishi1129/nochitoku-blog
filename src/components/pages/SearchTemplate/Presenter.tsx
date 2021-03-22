4bfc2b455c4859ef54916243c2df7873d67bd024
yuki <yuki.onishi1129@gmail.com>
2021-03-23 07:57:37 +0900

    update: repair pagetitle

--- src/components/pages/SearchTemplate/Presenter.tsx
+++ src/components/pages/SearchTemplate/Presenter.tsx
@@ -7,10 +7,13 @@ import React from 'react'
 /* components */
 import { BaseLayout } from '@/components/layouts/BaseLayout'
 import { BreadList } from '@/components/layouts/BreadList'
+import { PageTitle } from '@/components/common/atoms/PageTitle'
 // /* constants */
 // import { blogShowCount } from '@/constants/config'
 /* types */
 import { BlogItemType } from '@/types/blog'
+/* styles*/
+import styles from './styles.module.scss'
 
 /**
  * props
@@ -28,7 +31,9 @@ export const Presenter: React.FC<Props> = (props: Props) => {
   const { breadName } = props
   return (
     <BaseLayout breadName={breadName}>
-      <div>aaa</div>
+      <div className={styles.container}>
+        <PageTitle title="検索結果" />
+      </div>
     </BaseLayout>
   )
 }
