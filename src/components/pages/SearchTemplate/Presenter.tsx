d02458a1fb85fe8d9462dd010ce7f58f60a68f9a
yuki <yuki.onishi1129@gmail.com>
2021-03-23 09:39:31 +0900

    update: refactor

--- src/components/pages/SearchTemplate/Presenter.tsx
+++ src/components/pages/SearchTemplate/Presenter.tsx
@@ -4,18 +4,14 @@
  * @package Component
  */
 import React from 'react'
-import { useRouter } from 'next/router'
 /* components */
 import { BaseLayout } from '@/components/layouts/BaseLayout'
 import { PageTitle } from '@/components/common/atoms/PageTitle'
 import { SearchInputForm } from '@/components/common/molcules/SearchInputForm'
 import { SearchBlogItem } from './organisms/SearchBlogItem'
-/* contexts */
-import { useBlogState } from '@/contexts/BlogContext'
-// /* constants */
-// import { blogShowCount } from '@/constants/config'
 /* types */
 import { BlogItemType } from '@/types/blog'
+import { EventType } from '@/types/event'
 /* styles*/
 import styles from './styles.module.scss'
 
@@ -24,6 +20,9 @@ import styles from './styles.module.scss'
  */
 type Props = {
   breadName: string
+  searchText: string
+  showBlogList: BlogItemType[]
+  onChange: EventType['onChange']
 }
 
 /**
@@ -32,36 +31,7 @@ type Props = {
  * @returns
  */
 export const Presenter: React.FC<Props> = (props: Props) => {
-  const { breadName } = props
-  const { blogList } = useBlogState()
-  const router = useRouter()
-
-  let queryText = ''
-
-  if (router?.query?.keyword && typeof router.query.keyword === 'string') {
-    queryText = router.query.keyword
-  }
-
-  const [searchText, setSearchText] = React.useState(queryText)
-  const [showBlogList, setShowBlogList] = React.useState(blogList)
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value)
-
-    const searctBlogList = blogList.filter((blog) => {
-      return blog.title.indexOf(e.target.value) > -1
-    })
-
-    setShowBlogList(searctBlogList)
-  }
-
-  React.useEffect(() => {
-    const searctBlogList = blogList.filter((blog) => {
-      return blog.title.indexOf(queryText) > -1
-    })
-
-    setShowBlogList(searctBlogList)
-  }, [queryText, blogList])
+  const { breadName, searchText, showBlogList, onChange } = props
 
   return (
     <BaseLayout breadName={breadName}>
--- src/components/pages/SearchTemplate/index.tsx
+++ src/components/pages/SearchTemplate/index.tsx
@@ -4,8 +4,11 @@
  * @package Component
  */
 import React from 'react'
+import { useRouter } from 'next/router'
 /* components */
 import { Presenter } from './Presenter'
+/* contexts */
+import { useBlogState } from '@/contexts/BlogContext'
 
 /**
  * props
@@ -21,6 +24,46 @@ type Props = {
  */
 export const SearchTemplate: React.FC<Props> = (props: Props) => {
   const { breadName } = props
+  const { blogList } = useBlogState()
+  const router = useRouter()
 
-  return <Presenter breadName={breadName} />
+  // 初期検索キーワード
+  let queryText = ''
+  if (router?.query?.keyword && typeof router.query.keyword === 'string') {
+    queryText = router.query.keyword
+  }
+
+  // 検索ページで動的に変更する検索キーワード
+  const [searchText, setSearchText] = React.useState(queryText)
+  // 検索キーワードにHitしたブログ記事一覧
+  const [showBlogList, setShowBlogList] = React.useState(blogList)
+
+  // 動的検索キーワード更新処理
+  // 更新時にブログリストの検索も同時実行
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+
+    const searctBlogList = blogList.filter((blog) => {
+      // キーワード部分一致
+      return blog.title.indexOf(e.target.value) > -1
+    })
+    setShowBlogList(searctBlogList)
+  }
+
+  React.useEffect(() => {
+    // 画面遷移時のみurlのgetで渡ってきたキーワードで検索
+    const searctBlogList = blogList.filter((blog) => {
+      return blog.title.indexOf(queryText) > -1
+    })
+    setShowBlogList(searctBlogList)
+  }, [queryText, blogList])
+
+  return (
+    <Presenter
+      breadName={breadName}
+      searchText={searchText}
+      showBlogList={showBlogList}
+      onChange={onChange}
+    />
+  )
 }
