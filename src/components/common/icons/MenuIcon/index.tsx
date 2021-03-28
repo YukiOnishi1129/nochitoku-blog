6d2289d9bc4d3097ae3f2b059e9ec05487365c6b
yuki <yuki.onishi1129@gmail.com>
2021-03-28 20:56:39 +0900

    add: MenuIcon component

---
+++ src/components/common/icons/MenuIcon/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * common/icons/MenuIcon
+ * @package Component
+ */
+import React from 'react'
+
+/**
+ * props
+ */
+export type MenuIconProps = {
+  size?: number
+  color?: string
+}
+
+/**
+ * MenuIcon
+ * @param props MenuIconProps
+ * @returns
+ */
+export const MenuIcon: React.FC<MenuIconProps> = (props: MenuIconProps) => (
+  <svg
+    width={props.size || 25}
+    height={props.size || 28}
+    viewBox="0 0 25 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.16667 7H20.8333V9.33333H4.16667V7ZM8.33333 12.8333H20.8333V15.1667H8.33333V12.8333ZM13.5417 18.6667H20.8333V21H13.5417V18.6667Z"
+      fill={props.color || '#423a57'}
+    />
+  </svg>
+)
