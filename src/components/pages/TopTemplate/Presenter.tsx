7cf421661205894ca93fcabefe8e46bf9e6d82a5
yuki <yuki.onishi1129@gmail.com>
2021-03-29 21:30:28 +0900

    add: MetaHead component

--- src/components/pages/TopTemplate/Presenter.tsx
+++ src/components/pages/TopTemplate/Presenter.tsx
@@ -4,6 +4,7 @@
  * @package Component
  */
 import React from 'react'
+import Head from 'next/head'
 /* components */
 import { BasePostPageLayout } from '@/components/layouts/BasePostPageLayout'
 import { BlogItem } from '@/components/common/molcules/BlogItem'
@@ -33,30 +34,35 @@ export const Presenter: React.FC<Props> = (props: Props) => {
   const { blogList, totalCount } = props
 
   return (
-    <BasePostPageLayout>
-      {/* ブログ記事一覧表示 */}
-      <div className={styles.blogItem}>
-        {blogList.length > 0 &&
-          blogList.map((blogItem, index) => (
-            <BlogItem key={`${blogItem.id}_${index}`} blogItem={blogItem} />
-          ))}
-      </div>
+    <>
+      {/* <Head>
+        <title>aaaaaaaaaa</title>
+      </Head> */}
+      <BasePostPageLayout>
+        {/* ブログ記事一覧表示 */}
+        <div className={styles.blogItem}>
+          {blogList.length > 0 &&
+            blogList.map((blogItem, index) => (
+              <BlogItem key={`${blogItem.id}_${index}`} blogItem={blogItem} />
+            ))}
+        </div>
 
-      {/* ブログ記事一覧表示 レスポンシブ*/}
-      <div className={styles.blogItem__responsive}>
-        {blogList.length > 0 &&
-          blogList.map((blogItem, index) => (
-            <BlogItemResponsive
-              key={`${blogItem.id}_${index}`}
-              blogItem={blogItem}
-            />
-          ))}
-      </div>
+        {/* ブログ記事一覧表示 レスポンシブ*/}
+        <div className={styles.blogItem__responsive}>
+          {blogList.length > 0 &&
+            blogList.map((blogItem, index) => (
+              <BlogItemResponsive
+                key={`${blogItem.id}_${index}`}
+                blogItem={blogItem}
+              />
+            ))}
+        </div>
 
-      {/* ページネーション */}
-      {totalCount / blogShowCount > 1 && (
-        <Pagination totalCount={totalCount} link="/page/" />
-      )}
-    </BasePostPageLayout>
+        {/* ページネーション */}
+        {totalCount / blogShowCount > 1 && (
+          <Pagination totalCount={totalCount} link="/page/" />
+        )}
+      </BasePostPageLayout>
+    </>
   )
 }
