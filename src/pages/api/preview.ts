543486b4d36b142a33a674b0b0c545475b31367b
yuki <yuki.onishi1129@gmail.com>
2021-03-31 23:45:54 +0900

    content.data.id

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -33,7 +33,7 @@ const preview = async (req: NextApiRequest, res: NextApiResponse) => {
     draftKey: req.query.draftKey,
   })
   // @ts-ignore
-  res.writeHead(307, { Location: `/${req.query.slug}` })
+  res.writeHead(307, { Location: `/${content.data.id}` })
   res.end('Preview mode enabled')
 }
 
