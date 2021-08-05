b3ed37e87455f76dbf87fafb16ee88f8282b25c6
yuki <yuki.onishi1129@gmail.com>
2021-08-05 23:36:35 +0900

    repair: SearchBlogItem

--- src/components/pages/SearchTemplate/index.tsx
+++ src/components/pages/SearchTemplate/index.tsx
@@ -22,7 +22,7 @@ type Props = {
 }
 
 /**
- * container
+ * SearchTemplate
  * @param {Props} props
  * @returns
  */
--- src/components/pages/SearchTemplate/organisms/SearchBlogItem/index.tsx
+++ src/components/pages/SearchTemplate/organisms/SearchBlogItem/index.tsx
@@ -1,14 +1,17 @@
 /**
  * SearchTemplate/organisms/SearchBlogItem
- * ContainerComponent
  * @package Component
  */
 import React from 'react'
-/* components */
-import { Presenter } from './Presenter'
+import Link from 'next/link'
+import Image from 'next/image'
+/*　component */
+import { DateArea } from '@/components/common/molecules/DateArea'
 /* types */
 import { BlogItemType } from '@/types/blog'
 import { ImageType } from '@/types/image'
+/* styles */
+import styles from './styles.module.scss'
 
 /**
  * props
@@ -18,18 +21,50 @@ type Props = {
 }
 
 /**
- * container
- * @param props Props
+ * SearchBlogItem
+ * @param {Props} props
  * @returns
  */
 export const SearchBlogItem: React.FC<Props> = (props: Props) => {
+  /* props */
   const { blogItem } = props
 
-  const propsImage: ImageType = {
+  const image: ImageType = {
     url: blogItem?.image?.url ? blogItem.image.url : '/no_image.png',
     width: blogItem?.image?.width ? blogItem.image.width : 750,
     height: blogItem?.image?.height ? blogItem.image.height : 422,
   }
 
-  return <Presenter blogItem={blogItem} image={propsImage} />
+  return (
+    <Link href="/[blogId]" as={`/${blogItem.id}`}>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <Image
+            src={image.url}
+            alt="Picture"
+            width={image.width * 2}
+            height={image.height * 2}
+          />
+        </div>
+        <h2 className={styles.title}>{blogItem.title}</h2>
+
+        <div className={styles.category}>
+          {blogItem.categories.map((category, index) => {
+            return (
+              <div
+                className={styles.category__item}
+                key={`${category.id}_${index}`}
+              >
+                {category.name}
+              </div>
+            )
+          })}
+        </div>
+
+        <div className={styles.date}>
+          <DateArea date={blogItem.createdAt} />
+        </div>
+      </div>
+    </Link>
+  )
 }
