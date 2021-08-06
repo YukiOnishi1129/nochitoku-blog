c3a78b347e8925db7f05ef5e698426d32a6590a9
yuki <yuki.onishi1129@gmail.com>
2021-08-06 15:18:11 +0900

    update: modal components hooks

---
+++ src/components/modals/MenuModal/useMenuModal.tsx
@@ -0,0 +1,65 @@
+/**
+ * useMenuModal
+ * @package Hooks
+ */
+import React from 'react'
+import { useRouter } from 'next/router'
+/* constants */
+import { NAVIGATION_LINK } from '@/constants/navigation'
+
+/**
+ * interface
+ */
+interface HooksParam {
+  handleCloseMenuModal: () => void
+}
+
+/**
+ * useMenuModal
+ * @param {HooksParam} param
+ * @returns
+ */
+export const useMenuModal = (param: HooksParam) => {
+  /* params */
+  const { handleCloseMenuModal } = param
+  /* router */
+  const router = useRouter()
+
+  /**
+   * 「ホーム」画面遷移処理
+   */
+  const handleHomeLink = React.useCallback(() => {
+    router.push({
+      pathname: `${NAVIGATION_LINK.TOP}`,
+    })
+    handleCloseMenuModal()
+  }, [router, handleCloseMenuModal])
+
+  /**
+   * 「このブログについて」画面遷移処理
+   */
+  const handleAboutLink = React.useCallback(() => {
+    router.push({
+      pathname: `${NAVIGATION_LINK.ABOUT}`,
+    })
+    handleCloseMenuModal()
+  }, [router, handleCloseMenuModal])
+
+  /**
+   * 「プロフィール」画面遷移処理
+   */
+  const handleProfileLink = React.useCallback(() => {
+    router.push({
+      pathname: `${NAVIGATION_LINK.PROFILE}`,
+    })
+    handleCloseMenuModal()
+  }, [router, handleCloseMenuModal])
+
+  return {
+    action: {
+      handleHomeLink,
+      handleAboutLink,
+      handleProfileLink,
+    },
+  }
+}
