e6fee3b15435f66bcf1d0db0a1c96f4591bb99b4
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-11-08 21:09:29 +0900

    update: pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withPWA = require('next-pwa')
+/**
+ * @type {import('next').NextConfig}
+ */
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+})
 
-const nextConfig = withPWA({
+const nextConfig = {
   env: {
     X_API_KEY: process.env.X_API_KEY,
   },
@@ -10,6 +20,9 @@ const nextConfig = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public/sw', // swの出力ディレクトリ
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
     // runtimeCaching: []
   },
   webpack: (config) => {
@@ -17,6 +30,6 @@ const nextConfig = withPWA({
     // config.resolve.alias["~"] = resolve(__dirname, "src");
     return config
   },
-})
+}
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
